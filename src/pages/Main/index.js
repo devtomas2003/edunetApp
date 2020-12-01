625ec651b6ad9a118a78505c6877303567d00340
devtomas2003 <54170951+devtomas2003@users.noreply.github.com>
2020-12-01 12:49:53 +0000

    Correção de tamanhos

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -10,8 +10,8 @@ import { PanGestureHandler, State } from 'react-native-gesture-handler';
 import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles';
 
 export default function Main(){
+    let offset = 0;
     const translateY = new Animated.Value(0);
-
     const animatedEvent = Animated.event(
         [
             {
@@ -24,7 +24,27 @@ export default function Main(){
     );
 
     function onHandlerStateChange(event){
-
+        if(event.nativeEvent.oldState === State.ACTIVE){
+            let opened = false;
+            const { translationY } = event.nativeEvent;
+            offset += translationY;
+            if(translationY >= 100){
+                opened = true;
+            }else{
+                translateY.setValue(offset);
+                translateY.setOffset(0);
+                offset = 0;
+            }
+            Animated.timing(translateY, {
+                toValue: opened ? 310 : 0,
+                duration: 200,
+                useNativeDriver: true
+            }).start(() => {
+                offset = opened ? 310 : 0;
+                translateY.setOffset(offset);
+                translateY.setValue(0);
+            });
+        }
     }
 
     return (
@@ -39,8 +59,8 @@ export default function Main(){
                 <Card style={{
                     transform: [{
                         translateY: translateY.interpolate({
-                            inputRange: [-350, 0, 380],
-                            outputRange: [-50, 0, 380],
+                            inputRange: [-350, 0, 310],
+                            outputRange: [-50, 0, 310],
                             extrapolate: 'clamp'
                         })
                     }]
@@ -59,7 +79,7 @@ export default function Main(){
                 </Card>
                 </PanGestureHandler>
             </Content>
-            <Tabs />
+            <Tabs translateY={translateY} />
         </Container>
     );
 }
